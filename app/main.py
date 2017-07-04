63d1d86d9b8f9fc1759bf05572f2753b1a744539
Jaime Alberto Sanchez Hidalgo <jaime@grupocitec.com>
2017-07-04 16:45:52 -0300

    Versão 4

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_workd():
-    return '33333333333333333333333'
+    return '4444444444444444444444444444444'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
